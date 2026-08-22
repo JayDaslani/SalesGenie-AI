8af08d510fa759e3f76b972591285c4d8580a109
JayDaslani <jaydashlani@gmail.com>
2026-08-22 14:07:03 +0530

    Created agent and tools

---
+++ Backend/ai_pipeline/agent.py
@@ -0,0 +1,138 @@
+from langgraph.graph import StateGraph, END
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, ToolMessage
+from typing import TypedDict, List, Annotated
+import operator
+import os
+from dotenv import load_dotenv
+
+from tools import analyze_sales, get_forcast, get_customer_segments, get_quick_stats
+
+load_dotenv()
+
+llm = ChatGroq(
+    model='qwen/qwen3.6-27b', 
+    api_key=os.getenv('GROQ_API_KEY'),
+    temperature=0.3
+)
+tools = [
+    analyze_sales,
+    get_forcast,
+    get_customer_segments,
+    get_quick_stats
+]
+
+tools_map = {t.name: t for t in tools}
+llm_with_tools = llm.bind_tools(tools)
+
+class AgentState(TypedDict):
+    messages: Annotated[List, operator.add]
+
+def llm_node(state: AgentState):
+    system = SystemMessage(content="""
+You are SalesGenie AI - an Intelligent 
+sales analytics assistant.
+
+You help users understand their sales data.
+        
+Available tools:
+- analyze_sales: Sales analysis
+- get_forcast: Future Predictions
+- get_customer_segments: Customer info
+- get_quick_stats: Quick metrics
+                           
+Always use tools to get data.
+Then explain insights clearly.
+Be concise and business-focused.
+""")
+    
+    messages = [system] + state['messages']
+    response = llm_with_tools.invoke(messages)
+    return {"messages": [response]}
+
+def tool_node(state: AgentState):
+    last_message = state['messages'][-1]
+    results = []
+
+    for tool_call in last_message.tool_calls:
+        tool_name = tool_call['name']
+        tool_args = tool_call['args']
+
+        if tool_name in tools_map:
+            result = tools_map[tool_name].invoke(tool_args)
+        else:
+            result = f"Tool not found : {tool_name}"
+
+        results.append(
+            ToolMessage(content=str(result), tool_call_id=tool_call['id'])
+        )
+    return {'messages': results}
+
+def should_use_tool(state: AgentState):
+    last = state['messages'][-1]
+    if (hasattr(last, 'tool_calls') and last.tool_calls):
+        return "tool"
+    return "end"
+
+memory = MemorySaver()
+
+graph = StateGraph(AgentState)
+graph.add_node('llm', llm_node)
+graph.add_node('tool', tool_node)
+
+graph.set_entry_point('llm')
+
+graph.add_conditional_edges(
+    'llm',
+    should_use_tool,
+    {'tool': 'tool', 'end': END}
+)    
+
+graph.add_edge('tool', 'llm')
+
+agent_app = graph.compile(checkpointer=memory)
+
+def chat(message: str, session_id: str='default') -> str:
+    config = {
+        "configurable": {
+            'thread_id': session_id
+        }
+    }
+
+    result = agent_app.invoke(
+        {
+            'messages': [
+                HumanMessage(content=message)
+            ]
+        },
+        config=config
+    )
+
+    for msg in reversed(result['messages']):
+        if (isinstance(msg, AIMessage) and msg.content):
+            return msg.content
+
+    return "No response generated"
+
+print("=== SalesGenie AI Agent ===")
+
+tests = [
+    'What is the total revenue ?',
+    'Which is the best-performing region?',
+    'Give me the forecast for the next 3 months.',
+    'Explain the Customer Segments.',
+    'Which Category is the best ?'
+]
+
+for q in tests:
+    print(f"Q : {q}")
+    response = chat(q)
+    print(f"A: {response[:200]}\n")
+    print('-'*40)
+
+
+
+
+    
+
---
+++ Backend/ai_pipeline/tools.py
@@ -0,0 +1,207 @@
+import pandas as pd
+import numpy as np
+import json
+import os
+import pickle
+from langchain_core.tools import tool
+
+df = pd.read_csv('/Users/jaydasalani/Desktop/SalesGenie AI/data/train.csv', encoding='latin-1')
+
+df['Order Date'] = pd.to_datetime(df['Order Date'], format='%d/%m/%Y')
+
+rfm = pd.read_csv('/Users/jaydasalani/Desktop/SalesGenie AI/data/customer_segments.csv')
+
+with open('/Users/jaydasalani/Desktop/SalesGenie AI/data/model_info.json') as f:
+    model_info = json.load(f)
+
+with open('/Users/jaydasalani/Desktop/SalesGenie AI/data/best_model-2.pkl', 'rb') as f:
+    forecast_model = pickle.load(f)
+
+
+@tool
+def analyze_sales(query: str) -> str:
+    """
+    Analyzes Sales data. 
+    Use this When you have question about Sales.
+    Example: "best region", "top prodcut", "monthly trend"
+    """
+
+    query = query.lower()
+    results = []
+
+    total = df['Sales'].sum()
+    results.append(f"Total : ${total:,.2f}")
+
+    best_cat = df.groupby('Category')['Sales'].sum().idxmax()
+    cat_sales = df.groupby('Category')['Sales'].sum().max()
+    results.append(f"Best Category : {best_cat}"
+                   f"(${cat_sales:,.2f})")
+    
+    best_reg = df.groupby('Region')['Sales'].sum().idxmax()
+    reg_sales = df.groupby('Region')['Sales'].sum().max()
+    results.append(f"Best Region : {best_reg}"
+                   f"(${reg_sales:,.2f})")
+    
+    best_sub = df.groupby('Sub-Category')['Sales'].sum().idxmax()
+    results.append(f"Best Sub Category : {best_sub}")
+
+    yearly = df.groupby(df['Order Date'].dt.year)['Sales'].sum()
+    for year, sales in yearly.items():
+        results.append(f"Year  {year} : ${sales:,.2f}")
+
+    return "\n".join(results)
+
+
+@tool
+def get_forcast(months: int = 3) -> str:
+    """
+    Predicts Future Sales.
+    Use this when you need a future forecast.
+    Input: How many months to predict.
+    """
+
+    try:
+        monthly = pd.read_csv('/Users/jaydasalani/Desktop/SalesGenie AI/data/monthly_features.csv')
+
+        features = model_info['selected_features']
+
+        available = [f for f in features if f in monthly.columns]
+
+        data = monthly[available + ['Total_Sales']].dropna()
+
+        last_row = data.iloc[-1]
+        last_sales = data['Total_Sales'].values
+
+        predictions = []
+
+        for i in range(months):
+            next_month = int(last_row['Month'] + i) % 12 + 1
+            next_year = int(last_row['Year']) + (int(last_row['Month']+i)//12) 
+            next_feat = {}
+            for f in available:
+              if f == 'Year':
+                  next_feat[f] = next_year
+              elif f == 'Month':
+                  next_feat[f] = next_year
+              elif f == 'Quarter':
+                  next_feat[f] = ((next_month-1)//3)+1
+              elif f == 'Month_Sin':
+                  next_feat[f] = np.sin(2*np.pi*next_month/12)
+              elif f == 'Month_Cos':
+                  next_feat[f] = np.cos(2*np.pi*next_month/12)
+              elif f == 'Is_Holiday_Month':
+                  next_feat[f] = int(next_month in [11, 12, 1])
+              elif f == 'Is_Quarter_END':
+                  next_feat[f] = int(next_month in [3, 6, 9, 12])
+              elif f == 'Lag_1':
+                  next_feat[f] = last_sales[-1]
+              elif f == 'Lag_2':
+                  next_feat[f] = last_sales[-2]
+              elif f == 'Lag_3':
+                  next_feat[f] = last_sales[-3]
+              elif f == 'Rolling_3':
+                  next_feat[f] = np.mean(last_sales[-3:])
+        
+
+            pred = forecast_model.predict(pd.DataFrame([next_feat]))[0]
+            predictions.append({
+               'month': next_month,
+               'year': next_year,
+               'prediction': pred
+
+            })
+
+        results = [f"Sales Forecast (Next {months} months):"]
+
+        for p in predictions:
+            results.append(
+                    f"  {p['year']}-"
+                    f"{p['month']:02d}: "
+                    f"${p['prediction']:,.2f}"
+            )
+        return "\n".join(results)
+    
+    except Exception as e:
+        return f"Forecast error : {str(e)}"
+    
+
+@tool
+def get_customer_segments(query: str = 'all') -> str:
+    """Identifies customer segments.
+    Use this when you need customer information.
+    Categories include : Champions, Loyal, At Risk and Lost.
+    """
+    result = ['Customer Segment Analysis : ']
+
+    seg_count = rfm['Segment'].value_counts()
+
+    for seg, count in seg_count.items():
+        seg_data = rfm[rfm['Segment'] == seg]
+        avg_monetary = seg_data['Monetary'].mean()
+        total_rev = seg_data['Monetary'].sum()
+
+        result.append(
+            f"\n{seg}:"
+            f"\n  Count: {count}"
+            f"\n  Avg Revenue: "
+            f"${avg_monetary:,.2f}"
+            f"\n  Total Revenue: "
+            f"${total_rev:,.2f}"
+        )
+
+    return "\n".join(result)
+
+
+@tool
+def get_quick_stats(metric: str) -> str:
+    """
+    Provides quick statistics.
+    Use this when you need a specefic metric.
+    Metrics : revenue, orders, customers, products, regions
+    """
+
+    metric = metric.lower()
+
+    if 'revenue' in metric or 'sales' in metric:
+        total = df['Sales'].sum()
+        avg = df['Sales'].mean()
+        max_sale = df['Sales'].max()
+
+        return (
+            f"Revenue Stats:\n"
+            f"  Total: ${total:,.2f}\n"
+            f"  Average Order: ${avg:,.2f}\n"
+            f"  Max Order: ${max_sale:,.2f}"
+        )
+    
+    elif 'order' in metric:
+        total = df['Order ID'].nunique()
+        return f"Total Orders : {total:,}"
+    
+    elif 'customer' in metric:
+        total = df['Customer ID'].nunique()
+        return f"Total Customers: {total:,}"
+    
+    elif 'product' in metric:
+        total = df['Product ID'].nunique()
+        return f"Total Products : {total:,}"
+    
+    elif 'region' in metric:
+        regions = df.groupby('Region')['Sales'].sum().sort_values(ascending=False)
+        result = ['Regional Sales : ']
+        for reg, sales in regions.items():
+            result.append(
+                f"  {reg}: ${sales:,.2f}"
+            )
+        return "\n".join(result)
+    
+    else:
+        return (
+            "Availabel metrics : revenue, orders, customers, products, regions"
+        )
+
+
+
+
+
+    
